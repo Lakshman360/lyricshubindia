7dfda8feb02b4f2c06bbba1d95e2dc093f905e1a
Lakshman360 <116963377+Lakshman360@users.noreply.github.com>
2026-03-25 14:48:38 +0530

    Delete Multi Pg directory

--- Multi Pg/backend/routes/attendanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scanQrAttendance, getAttendanceStats } = require('../controllers/attendanceController');
-const { protect, ownerCheck } = require('../middleware/authMiddleware');
-
-router.post('/scan', protect, ownerCheck, scanQrAttendance);
-router.get('/stats/:hostelId', protect, ownerCheck, getAttendanceStats);
-
-module.exports = router;
